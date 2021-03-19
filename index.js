16e9aa1de51c1c53065b8de9ea32b8c5bc6d9efd
Eli <elize.san10@gmail.com>
2021-03-19 10:43:30 -0500

    start server in index.js file

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // start your server here
+
+const server = require("./api/server");
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log("Hello! Listening on port 5000");
+});
