1630ebe0a4c4da080ae2f5643add5623d0f0692e
Eli <elize.san10@gmail.com>
2021-03-19 12:50:22 -0500

    build boiler plate for all routers

--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,21 @@
 // build your `/api/tasks` router here
+// build your `/api/projects` router here
+const express = require("express");
+const Task = require("./model");
+
+const router = express.Router();
+
+router.get("/", (req, res, next) => {});
+
+router.post("/", (req, res, next) => {});
+
+router.use((err, req, res, next) => {
+  // eslint-disable-line
+  res.status(500).json({
+    sageAdvice: "Hey, you can do this. You know how to read errors",
+    error: err.message,
+    stack: err.stack,
+  });
+});
+
+module.exports = router;
