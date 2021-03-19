7af35cc8ae69641ebc4f476ca529704110e6225a
Eli <elize.san10@gmail.com>
2021-03-19 12:27:25 -0500

    connect routes to server in server.js

--- api/server.js
+++ api/server.js
@@ -1,1 +1,18 @@
 // build your server here and require it from index.js
+const express = require("express");
+
+// Routes Reference
+const ProjectRouter = require("./project/router");
+const ResourceRouter = require("./resource/router");
+const TaskRouter = require("./task/router");
+
+const server = express();
+
+server.use(express.json());
+
+// Server, use these routes for these endpoints
+server.use("/api/projects", ProjectRouter);
+server.use("/api/resources", ResourceRouter);
+server.use("/api/tasks", TaskRouter);
+
+module.exports = server;
